264718d610d599b562823a367aa46b313e2cdcef
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2019-10-25 07:48:44 +0200

    Add Usage Charts

---
+++ assets/js/usage-charts.js
@@ -0,0 +1,44 @@
+---
+---
+
+{% for interval in site.data.usage-charts.intervals %}
+fetchData("{{ interval.endpoint }}").then(data => {
+    const x = data.map(month => month.number);
+    const y = data.map(month => month.usage);
+    createChart("{{ interval.id }}-chart", x, y);
+  });
+{% endfor %}
+
+async function fetchData(interval) {
+  const root = "{{ site.root-endpoint }}";
+  const endpoint = "{{ site.data.usage-charts.endpoint }}";
+  const response = await fetch(`${root}${endpoint}/${interval}`);
+  return await response.json();
+}
+
+function createChart(name, xData, yData) {
+  var ctx = document.getElementById(name).getContext("2d");
+
+  var gradient = ctx.createLinearGradient(0, 0, 0, 450);
+  gradient.addColorStop(0, "rgba(40, 167, 69, 0.5)");
+  gradient.addColorStop(0.5, "rgba(40, 167, 69, 0.25)");
+  gradient.addColorStop(1, "rgba(40, 167, 69, 0)");
+
+  new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: xData,
+      datasets: [
+        {
+          data: yData,
+          backgroundColor: gradient
+        }
+      ]
+    },
+    options: {
+      legend: {
+        display: false
+      }
+    }
+  });
+}
